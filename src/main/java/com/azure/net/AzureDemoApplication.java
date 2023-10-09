590c79845a18ce41550e31b565d1e11d49e08760
omipatel <pomkarpatel@gmail.com>
2023-10-09 16:46:49 +0530

    First commit

---
+++ src/main/java/com/azure/net/AzureDemoApplication.java
@@ -0,0 +1,24 @@
+package com.azure.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoApplication.class, args);
+	}
+
+}
+//echo "# spring-azure" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/omipatel/spring-azure.git
+//git push -u origin main
+//…or push an existing repository from the command line
+//git remote add origin https://github.com/omipatel/spring-azure.git
+//git branch -M main
+//git push -u origin main*./
---
+++ src/main/java/com/azure/net/controller/AzureController.java
@@ -0,0 +1,22 @@
+package com.azure.net.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AzureController {
+	
+	
+	    @GetMapping("/message")
+	    public String getMessage() {
+	    	 
+	    	return "Spring Boot Deployee inAzure";
+	    }
+	
+	
+	
+	
+
+}
